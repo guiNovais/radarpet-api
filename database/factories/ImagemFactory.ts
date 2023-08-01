ad9bf41a5ac55046c4d55bf4fc44e06a2329ab0a
Guilherme Novais <guilhermenovais.sh@gmail.com>
2023-08-01 16:57:42 -0300

    :sparkles: Modelo imagem

---
+++ database/factories/ImagemFactory.ts
@@ -0,0 +1,8 @@
+import Imagem from 'App/Models/Imagem'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Imagem, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
